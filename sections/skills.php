cc8212213c975035220033ec441d1e4c35cfc535
Peter M <Peter.P.Maltby@gmail.com>
2017-02-24 16:00:04 +0000

    skills started work; mainly my SQL work done

--- sections/skills.php
+++ sections/skills.php
@@ -4,6 +4,23 @@
 <section id="skills">
   <div class="container">
     <h1>skills</h1>
-    <p>this section is WIP AF<p>
+    <div class="parent-block">
+      <div class="block">
+        <h2>Code</h2>
+
+      </div>
+      <div class="block">
+        <h2>langauges</h2>
+
+      </div>
+      <div class="block">
+        <h2>Tools</h2>
+
+      </div>
+      <div class="block">
+        <h2>Currently Learning</h2>
+
+      </div>
+    </div>
   </div>
 </section>
