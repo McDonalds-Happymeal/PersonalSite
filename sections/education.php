cebaf6bc0f1021ca2fe39837e38fe2ca1efe1747
Peter M <Peter.P.Maltby@gmail.com>
2017-02-26 15:17:03 +0000

    education section complete

--- sections/education.php
+++ sections/education.php
@@ -3,6 +3,34 @@
 <section id="education">
   <div class="container">
     <h1>Education</h1>
-    <p>WIP AF</p>
+
+    <div class="row">
+      <div class="col-md-4">
+        <h2>John leggott College</h2>
+        <p>Sep 15 - Mar 17</p>
+        <p>Scunthorpe</p>
+      </div>
+      <div class="col-md-8">
+        <p>Here i did lots of stuff. it was ok a guess and lots of fun; i hope you understand this is placeholder content. becuase it is.</p>
+        <ul>
+          <li>BTEC ICT level 3 Extended Diploma</li>
+          <li>Maths AS-Level</li>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+        <h2>John leggott College</h2>
+        <p>Sep 15 - Mar 17</p>
+        <p>Scunthorpe</p>
+      </div>
+      <div class="col-md-8">
+        <p>Here i did lots of stuff. it was ok a guess and lots of fun; i hope you understand this is placeholder content. becuase it is.</p>
+        <ul>
+          <li>BTEC ICT level 3 Extended Diploma</li>
+          <li>Maths AS-Level</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </section>
