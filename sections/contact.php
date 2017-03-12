8ed599f1c714c17f742678816c9a6bd1f992a2d7
Peter M <Peter.P.Maltby@gmail.com>
2017-03-12 15:23:42 +0000

    started on contact

--- sections/contact.php
+++ sections/contact.php
@@ -1,9 +1,20 @@
 <?php
 ?>
 
-<section>
+<section id="contact">
   <div class="container">
-    <h1 id="contact">Contact</h1>
-    <p>WIP AF</p>
+    <div class="form">
+      <h1>Contact</h1>
+      <from action="contact_form.php" method="post">
+        <input type="text" name="Name" placeholder="Your name" />
+        <input type="text" name="Email" placeholder="Your Email" />
+        <input type="text" name="Subject" placeholder="Subject" />
+        <input type="text" name="Message" placeholder="Your Message" />
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+    <div class="media">
+
+    </div>
   </div>
 <section>
